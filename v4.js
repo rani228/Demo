2c9fbdf6420fcb35bfb3a961dd8c3c815add8f64
rani228 <ranichoramale7@gmail.com>
2025-10-18 22:05:29 +0530

    initial commit

--- v4.js
+++ v4.js
@@ -2,3 +2,8 @@
 let str="I love javascript";
 let str2=str.split(' ').reverse().join(' ');
 console.log(str2);
+
+
+let str3="Hello javascript";
+let str4=str.split(' ').reverse().join(' ');
+console.log(str4);
