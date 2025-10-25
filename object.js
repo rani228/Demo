6b0c1afbaa309307ee79efd278e98d7189d1e022
rani228 <ranichoramale7@gmail.com>
2025-10-25 15:17:05 +0530

    initial commit

---
+++ object.js
@@ -0,0 +1,65 @@
+
+var student={
+    name:'rajani',
+    age:'21',
+    gender:'female',
+    subject:['AI','CP','DP'],
+    Education:'B.tech',
+    
+};
+
+console.log('total student data:',student);
+console.log('typeof:',typeof student);
+console.log(student.name);
+console.log('total keys:',Object.keys(student));
+console.log('total values:',Object.values(student));
+console.log(delete student.age);
+console.log(student);
+
+
+// var student={
+//     fname:'rani',
+//     lname:'choramale',
+//     rollno:'30',
+//     education:'B Tech',
+//     marks:['70','80','90'],
+//     hobby:'sleeping',
+//     grade:'A',
+
+// };
+// console.log(student);
+
+// // a dot notation
+// //read the value
+// // console.log(student.fname);
+
+// //square brackets notations
+// // console.log(student["lname"]);
+
+
+// //edit/modify the object
+// // student.fname="vaishanvi";
+// // student["lname"]="shinde";
+// // console.log(student);
+
+// //3.Adding new properties or method to the object
+// student.address="piliv";
+// student["mob"]="9021844023";
+// console.log(student);
+
+// //4.delete the property or method of the object
+
+// console.log(student);
+// delete student.hobby;
+// delete student["grade"];
+// console.log(student);
+
+
+// var student={
+//     name:'rani',
+// };
+// console.log(student);
+// console.log(typeof student );
+
+
+
---
+++ practise.js
@@ -0,0 +1,71 @@
+// types of datatype
+// their are two types of datatype:
+// 1.primitive datatype
+// // 2.non primitive datatype
+// // 1.number
+// var a=10;
+// var b='12';
+// console.log('value of a:',a);
+// console.log(typeof a);
+// console.log(typeof( a));
+// var test1=a.toString();
+// console.log(typeof test1);
+// var test2=10-'12';
+// console.log(test2);
+// 2.string
+
+// var firstname='rani';
+// console.log(typeof firstname);
+// console.log(firstname);
+// console.log(typeof (firstname));
+
+// 3.boolean
+// var test1=true;
+// var test2=false;
+// console.log(test1);
+// console.log(typeof test1);//boolean
+
+// var test3=test1.toString();
+// console.log(typeof test3);
+
+// var isvalid=true;
+// console.log(isvalid);
+// if(isvalid){
+//     console.log('online');
+
+// }else{
+//     console.log('offline');
+// }
+
+// 4.null
+// var test7=null;
+// console.log(typeof test7);//object
+
+// 5.undefined
+// var a;
+// var b=10;
+// console.log(typeof a);//undefined
+// console.log(a);//undefined
+// console.log(typeof undefined);//undefined
+// console.log(typeof b);//number
+// console.log(b);//10
+// 6.symbol (ES6)
+
+// 2point
+// non primitive datatype
+// 1.Array
+// var test9=['a','b','c','d'];
+// console.log(test9);
+// console.log(typeof test9);//object
+
+// 2.object
+// var person={
+//     name:'Aba',
+//     Age:45,
+//     Phoneno:'9021844023',
+
+// };
+// console.log(person);
+// console.log(typeof person);//object
+
+
