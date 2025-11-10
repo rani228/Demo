1628146ca98b9021e7acd9b66be03011f9971005
rani228 <ranichoramale7@gmail.com>
2025-11-10 08:10:20 +0530

    intital commit

---
+++ spreatedop.js
@@ -0,0 +1,27 @@
+// spread of the example of object ex1
+// let person = { name: "Ravi", age: 30 };
+// let newPerson = { ...person, city: "Pune" };//newPerson object to expand keypairvalue in person
+
+// console.log(newPerson); 
+// // { name: 'Ravi', age: 30, city: 'Pune' }
+
+// // // //spread of the example array ex2
+// let arr1=[1,2,3];
+// let arr2=[...arr1,4,5];
+// console.log(arr2);
+
+// //ex3
+// let arr1=[1,2,3];
+// let newArray1=arr1;
+// newArray1.push(5);
+// console.log(arr1);//original array got impacted
+// let newArray=[...arr1];//creating copyof original array
+// newArray.push(6);//new value addded to newArray
+// console.log(newArray);
+
+
+// let newArray2=[...arr1,7,8,9];
+// let newArray3=[7,8,9,...arr1];
+// console.log(newArray2);
+// console.log(newArray3);
+
